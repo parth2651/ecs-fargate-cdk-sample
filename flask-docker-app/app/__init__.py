99dc630c2c822d23da5d4eb257dcdb79b9ba1445
Parth Patel <parth2651@gmail.com>
2021-02-11 13:35:23 -0800

    added extra file

---
+++ flask-docker-app/app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+application = Flask(__name__)
+application.config.from_object('config')
+db = SQLAlchemy(application)
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+application = Flask(__name__)
+application.config.from_object('config')
+db = SQLAlchemy(application)
