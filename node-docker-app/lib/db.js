48b64a5ed1a86ad07f729c52350e01be719e9bad
Parth Patel <parth2651@gmail.com>
2021-02-16 16:50:57 -0800

    adding db.js

---
+++ node-docker-app/lib/db.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host:'ab3test.cw8kxh7es4kd.us-east-2.rds.amazonaws.com',
+	user:'admin',
+	password:'admin123',
+	database:'Ab3Test',
+    port     : 3306
+});
+connection.connect(function(error){
+	if(!!error) {
+		console.log(error);
+	} else {
+		console.log('Connected..!');
+	}
+});
+
+module.exports = connection;
