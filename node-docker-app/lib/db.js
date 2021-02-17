02621901d9c3a21ee9e22920f3c5c53acd49626e
Parth Patel <parth2651@gmail.com>
2021-02-16 20:20:41 -0800

    change connection to pool

--- node-docker-app/lib/db.js
+++ node-docker-app/lib/db.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host:'ab3test.cw8kxh7es4kd.us-east-2.rds.amazonaws.com',
-	user:'admin',
-	password:'admin123',
-	database:'Ab3Test',
-    port     : 3306
-});
+// var connection = mysql.createConnection({
+// 	host:'ab3test.cw8kxh7es4kd.us-east-2.rds.amazonaws.com',
+// 	user:'admin',
+// 	password:'admin123',
+// 	database:'Ab3Test',
+//     port     : 3306
+// });
 // connection.connect(function(error){
 // 	if(!!error) {
 // 		console.log(error);
@@ -13,5 +13,15 @@ var connection = mysql.createConnection({
 // 		console.log('Connected..!');
 // 	}
 // });
+var connection  = mysql.createPool({
+	connectionLimit : 100,
+ 	host:'ab3test.cw8kxh7es4kd.us-east-2.rds.amazonaws.com',
+ 	user:'admin',
+ 	password:'admin123',
+ 	database:'Ab3Test',
+	port     : 3306,
+ 	timeout: 20000
+  });
+
 
 module.exports = connection;
